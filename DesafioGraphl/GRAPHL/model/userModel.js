f2765609b63b4e53f81bb97eeab1d8677478ee60
abated <dario.tasa@gmail.com>
2022-12-27 21:36:15 -0300

    DesafioDeno

--- DesafioGraphl/GRAPHL/model/userModel.js
+++
@@ -1,8 +0,0 @@
-class Persona {
-    constructor(id, { nombre, edad }) {
-      this.id = id;
-      this.nombre = nombre;
-      this.edad = edad;
-    }
-  }
-module.exports = {Persona}
--- DesafioGraphl/model/DTOs/productos.DTOs.js
+++
@@ -1,9 +0,0 @@
-function productoDTO(producto, _id, fyh) {
-    return {
-        ...producto,
-        _id,
-        fyh
-    }
-}   
-
-module.exports = productoDTO
